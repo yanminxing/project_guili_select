4fd1eb8dcbe1541a30d31252944bf6c128174322
blizx <1194474469@qq.com>
2024-05-04 19:13:26 +0800

    feat(登录页面): 新增静态页面

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
-<script setup lang="ts">
-import variables from '@/styles/align.module.scss';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <SvgIcon name="home" color="red"></SvgIcon>
+  <div class="app-container">
     <router-view></router-view>
   </div>
 </template>
 
 <style scoped lang="scss">
-@import '@/styles/align.module.scss';
-.p {
-  color: $colorBl !important;
+.app-container {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,66 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Lock } from '@element-plus/icons-vue';
+
+const form = ref({
+  password: '111111',
+  username: 'admin'
+});
+</script>
 
 <template>
-  <div>login</div>
+  <el-container class="page-login">
+    <el-form :model="form" class="page-login-form">
+      <h1>某某平台</h1>
+      <h3>欢迎×××××平台</h3>
+      <el-form-item>
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="form.password" type="password" :prefix-icon="Lock" show-password></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </el-container>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.page-login {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 100%;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+
+  .el-row {
+    width: 100%;
+  }
+
+  &-form {
+    margin-right: 100px;
+    padding: 20px;
+    width: 500px;
+    //height: 400px;
+    color: white;
+    background: url('@/assets/images/login_form.png') no-repeat;
+    background-size: cover;
+
+    h1 {
+      font-size: 40px;
+    }
+    h3 {
+      font-weight: bold;
+      margin-top: 20px;
+    }
+    .el-form-item {
+      margin-top: 20px;
+    }
+    .el-button {
+      width: 100%;
+    }
+  }
+}
+</style>
